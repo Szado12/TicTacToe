b33458ba368b5e0ae62f449b223538408ea652ae
Szado12 <michal.pijanowski2@gmail.com>
2022-10-17 00:25:39 +0200

    s

---
+++ TicTacToe/TicTacToe/WebSockets/WebSocketsHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using TicTacToe.Interfaces;
+using TicTacToe.Models;
+
+namespace TicTacToe.WebSockets
+{
+  public class WebSocketsHub : Hub<IChatClient>
+  {
+    public async Task SendMessage(MessageModel messageModel)
+    {
+      await Clients.All.ReceiveMessage(messageModel);
+    }
+  }
+}
